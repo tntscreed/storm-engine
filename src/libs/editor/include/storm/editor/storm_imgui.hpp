e1dd3be99f5b45991a9e2930fd8ab02479a6c690
Hammie <sam@hammie.be>
2024-04-07 20:36:03 +0200

    Fix fmt::format build error

--- src/libs/editor/include/storm/editor/storm_imgui.hpp
+++ src/libs/editor/include/storm/editor/storm_imgui.hpp
@@ -13,7 +13,7 @@ namespace ImGui
 template <typename T, typename... Args>
 IMGUI_API void TextFmt(T &&fmt, const Args &...args)
 {
-    std::string str = fmt::format(std::forward<T>(fmt), args...);
+    std::string str = fmt::format(fmt::runtime(std::forward<T>(fmt)), args...);
     ImGui::TextUnformatted(str.c_str(), str.c_str() + str.length() );
 }
 
